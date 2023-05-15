12a5b4da84e8e301bb41a08bfb1fea5ef8c9bc8b
Jesse Newland <jesse@jnewland.com>
2023-05-15 17:54:48 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { GitHubScriptArguments } from "@urcomputeringpal/github-script-ts";
+
+export async function function1(args: GitHubScriptArguments): Promise<String> {
+  // const { github, context, core } = args;
+  // const { glob, io, exec, fetch } = args;
+  // ...
+  return "string";
+}
