93b047b7f2e88dd029cf17e46046fd53bbe40de5
Jesse Newland <jesse@jnewland.com>
2023-05-15 23:25:44 +0000

    summarize history

--- index.ts
+++ index.ts
@@ -1,8 +1,67 @@
 import { GitHubScriptArguments } from "@urcomputeringpal/github-script-ts";
 
-export async function function1(args: GitHubScriptArguments): Promise<String> {
-  // const { github, context, core } = args;
-  // const { glob, io, exec, fetch } = args;
-  // ...
-  return "string";
+export async function summarizeHistory(args: GitHubScriptArguments): Promise<void> {
+  const { github, context, core } = args;
+  if (github === undefined || context == undefined || core === undefined) {
+    throw new Error("")
+  }
+
+  // FIXME selectable 'created' param to ensure we're using the same time period
+  // FIXME paginate
+  // FIXME also lookup default branch stats for the each of the same
+  const successful = await github.rest.actions.listWorkflowRuns({
+    ...context.repo,
+    workflow_id: context.workflow,
+    conclusion: "success",
+    exclude_pull_requests: true,
+  });
+  const failure = await github.rest.actions.listWorkflowRuns({
+    ...context.repo,
+    workflow_id: context.workflow,
+    conclusion: "failure",
+    exclude_pull_requests: true,
+  });
+
+  const failureDurations = failure.data.workflow_runs.map((run) => {
+    const created = new Date(run.created_at);
+    const updated = new Date(run.updated_at);
+    const diff = updated.getTime() - created.getTime();
+    return Math.floor(diff / 1000);
+  });
+  
+  const failureDurationsSorted = failureDurations.sort((a, b) => a - b);
+  const failureDurations99thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.99)];
+  const failureDurations90thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.90)];
+  const failureDurations50thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.50)];
+
+  const successfulDurations = successful.data.workflow_runs.map((run) => {
+    const created = new Date(run.created_at);
+    const updated = new Date(run.updated_at);
+    const diff = updated.getTime() - created.getTime();
+    return Math.floor(diff / 1000);
+  });
+
+  const successfulDurationsSorted = successfulDurations.sort((a, b) => a - b);
+  const successfulDurations99thPercentile = successfulDurationsSorted[Math.floor(successfulDurationsSorted.length * 0.99)];
+  const successfulDurations90thPercentile = successfulDurationsSorted[Math.floor(successfulDurationsSorted.length * 0.90)];
+  const successfulDurations50thPercentile = successfulDurationsSorted[Math.floor(successfulDurationsSorted.length * 0.50)];
+
+
+  core.summary
+    .addHeading(`Success rate: ${Math.round(successful.data.total_count / (successful.data.total_count + failure.data.total_count) * 100)}%`)
+    .addTable([
+      [{data: 'Percentile', header: true}, {data: 'Success duration in seconds', header: true}],
+      ["99th", `${successfulDurations99thPercentile}`],
+      ["90th", `${successfulDurations90thPercentile}`],
+      ["50th", `${successfulDurations50thPercentile}`],
+    ])
+    .addHeading(`Failure rate: ${Math.round(failure.data.total_count / (successful.data.total_count + failure.data.total_count) * 100)}%`)
+    .addTable([
+      [{data: 'Percentile', header: true}, {data: 'Failure duration in seconds', header: true}],
+      ["99th", `${failureDurations99thPercentile}`],
+      ["90th", `${failureDurations90thPercentile}`],
+      ["50th", `${failureDurations50thPercentile}`],
+    ])
+
+  return;
 }
