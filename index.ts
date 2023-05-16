216576648a7e6901f93b67736e13e1d06820c9d7
Jesse Newland <jesse@jnewland.com>
2023-05-16 00:31:20 +0000

    dig

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { GitHubScriptArguments } from "@urcomputeringpal/github-script-ts";
+import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
 
 export async function summarizeHistory(args: GitHubScriptArguments): Promise<void> {
   const { github, context, core } = args;
@@ -9,22 +10,15 @@ export async function summarizeHistory(args: GitHubScriptArguments): Promise<voi
   // FIXME selectable 'created' param to ensure we're using the same time period
   // FIXME paginate
   // FIXME also lookup default branch stats for the each of the same
+  let successful: RestEndpointMethodTypes["actions"]["listWorkflowRuns"]["response"];
   try {
-
-    const successful = await github.rest.actions.listWorkflowRuns({
+    successful = await github.rest.actions.listWorkflowRuns({
       ...context.repo,
       workflow_id: context.workflow,
       conclusion: "success",
       exclude_pull_requests: true,
     });
 
-    const failure = await github.rest.actions.listWorkflowRuns({
-      ...context.repo,
-      workflow_id: context.workflow,
-      conclusion: "failure",
-      exclude_pull_requests: true,
-    });
-  
     const successfulDurations = successful.data.workflow_runs.map((run) => {
       const created = new Date(run.created_at);
       const updated = new Date(run.updated_at);
@@ -36,46 +30,61 @@ export async function summarizeHistory(args: GitHubScriptArguments): Promise<voi
     const successfulDurations99thPercentile = successfulDurationsSorted[Math.floor(successfulDurationsSorted.length * 0.99)];
     const successfulDurations90thPercentile = successfulDurationsSorted[Math.floor(successfulDurationsSorted.length * 0.90)];
     const successfulDurations50thPercentile = successfulDurationsSorted[Math.floor(successfulDurationsSorted.length * 0.50)];  
-
-    core.summary
-      .addHeading(`Success rate: ${Math.round(successful.data.total_count / (successful.data.total_count + failure.data.total_count) * 100)}%`)
-      .addTable([
-        [{data: 'Percentile', header: true}, {data: 'Success duration in seconds', header: true}],
-        ["99th", `${successfulDurations99thPercentile}`],
-        ["90th", `${successfulDurations90thPercentile}`],
-        ["50th", `${successfulDurations50thPercentile}`],
-      ]).
-    write()
-
-    const failureDurations = failure.data.workflow_runs.map((run) => {
-      const created = new Date(run.created_at);
-      const updated = new Date(run.updated_at);
-      const diff = updated.getTime() - created.getTime();
-      return Math.floor(diff / 1000);
-    });
     
-    const failureDurationsSorted = failureDurations.sort((a, b) => a - b);
-    const failureDurations99thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.99)];
-    const failureDurations90thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.90)];
-    const failureDurations50thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.50)];
+    let failure: RestEndpointMethodTypes["actions"]["listWorkflowRuns"]["response"];
+    try {
+      failure = await github.rest.actions.listWorkflowRuns({
+        ...context.repo,
+        workflow_id: context.workflow,
+        conclusion: "failure",
+        exclude_pull_requests: true,
+      });
 
-    core.summary
-      .addHeading(`Failure rate: ${Math.round(failure.data.total_count / (successful.data.total_count + failure.data.total_count) * 100)}%`)
-      .addTable([
-      [{data: 'Percentile', header: true}, {data: 'Failure duration in seconds', header: true}],
-      ["99th", `${failureDurations99thPercentile}`],
-      ["90th", `${failureDurations90thPercentile}`],
-      ["50th", `${failureDurations50thPercentile}`],
-    ])
+      core.summary
+        .addHeading(`Success rate: ${Math.round(successful.data.total_count / (successful.data.total_count + failure.data.total_count) * 100)}%`)
+        .addTable([
+          [{data: 'Percentile', header: true}, {data: 'Success duration in seconds', header: true}],
+          ["99th", `${successfulDurations99thPercentile}`],
+          ["90th", `${successfulDurations90thPercentile}`],
+          ["50th", `${successfulDurations50thPercentile}`],
+        ]).
+      write();
+  
+      const failureDurations = failure.data.workflow_runs.map((run) => {
+        const created = new Date(run.created_at);
+        const updated = new Date(run.updated_at);
+        const diff = updated.getTime() - created.getTime();
+        return Math.floor(diff / 1000);
+      });
+      
+      const failureDurationsSorted = failureDurations.sort((a, b) => a - b);
+      const failureDurations99thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.99)];
+      const failureDurations90thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.90)];
+      const failureDurations50thPercentile = failureDurationsSorted[Math.floor(failureDurationsSorted.length * 0.50)];
+  
+      core.summary
+        .addHeading(`Failure rate: ${Math.round(failure.data.total_count / (successful.data.total_count + failure.data.total_count) * 100)}%`)
+        .addTable([
+        [{data: 'Percentile', header: true}, {data: 'Failure duration in seconds', header: true}],
+        ["99th", `${failureDurations99thPercentile}`],
+        ["90th", `${failureDurations90thPercentile}`],
+        ["50th", `${failureDurations50thPercentile}`],
+      ])
+  
+    } catch {
+      core.summary
+        .addHeading(`Success rate: 100%`)
+        .addTable([
+          [{data: 'Percentile', header: true}, {data: 'Success duration in seconds', header: true}],
+          ["99th", `${successfulDurations99thPercentile}`],
+          ["90th", `${successfulDurations90thPercentile}`],
+          ["50th", `${successfulDurations50thPercentile}`],
+        ]).
+      write();
+    }
 
-  } catch(err) {
-    core.setFailed(`${err}`);
+  } catch {
+    core.notice('No successful workflows found');
   }
-
-
-
-
-
-
   return;
 }
