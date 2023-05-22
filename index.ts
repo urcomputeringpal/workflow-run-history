b13578e7460a3c12c9492d8ec972f0952d6eb67a
Jesse Newland <jesse@jnewland.com>
2023-05-22 12:14:36 +0000

    whoops

--- index.ts
+++ index.ts
@@ -7,21 +7,21 @@ function describePercentile(percentile: number, suffix: string): string {
     } else if (percentile == 50) {
         return `👍 Perfectly average among ${suffix}`;
     } else if (percentile == 0) {
-        return `🐌🐌🐌 Slower than all ${suffix}`;
-    } else if (percentile >= 99) {
+        return `🥱🥱🥱 Slower than all ${suffix}`;
+    } else if (percentile >= 90) {
         return `🐎🐎🐎 Faster than ${percentile}% of ${suffix}`;
-    } else if (percentile <= 99 && percentile >= 90) {
+    } else if (percentile <= 90 && percentile >= 80) {
         return `🐎🐎 Faster than ${percentile}% of ${suffix}`;
-    } else if (percentile < 90 && percentile > 75) {
+    } else if (percentile < 80 && percentile >= 70) {
         return `🐎 Faster than ${percentile}% of ${suffix}`;
-    } else if (percentile < 75 && percentile > 40) {
+    } else if (percentile < 70 && percentile >= 40) {
         return `👍 Faster than ${percentile}% of ${suffix}`;
-    } else if (percentile < 40 && percentile > 25) {
-        return `Slower than ${100 - percentile}% of ${suffix}`;
-    } else if (percentile <= 25 && percentile > 10) {
+    } else if (percentile < 40 && percentile >= 20) {
         return `🐌 Slower than ${100 - percentile}% of ${suffix}`;
-    } else if (percentile <= 10) {
-        return `🐌🐌 Slower than ${100 - percentile}% of ${suffix}`;
+    } else if (percentile <= 20 && percentile >= 10) {
+        return `🐌 Slower than ${100 - percentile}% of ${suffix}`;
+    } else {
+        return `🐌🐌🐌 Slower than ${100 - percentile}% of ${suffix}`;
     }
 }
 
