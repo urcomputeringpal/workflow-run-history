d53ad16a5c66c9342332ae3ccbb7e4db6aa2f172
Jesse Newland <jesse@jnewland.com>
2023-05-25 00:34:14 +0000

    try to keep this reasonable

--- index.ts
+++ index.ts
@@ -1,67 +1,7 @@
 import { GitHubScriptArguments } from "@urcomputeringpal/github-script-ts";
 import { getWorkflowRuns } from "./src/workflowGroup";
-
-function describePercentile(percentile: number, suffix: string): string {
-    if (percentile == 100) {
-        return `💎💎💎 Faster than all ${suffix}`;
-    } else if (percentile == 50) {
-        return `👍 Perfectly average among ${suffix}`;
-    } else if (percentile == 0) {
-        return `🥱🥱🥱 Slower than all ${suffix}`;
-    } else if (percentile >= 90) {
-        return `🐎🐎🐎 Faster than ${percentile}% of ${suffix}`;
-    } else if (percentile <= 90 && percentile >= 80) {
-        return `🐎🐎 Faster than ${percentile}% of ${suffix}`;
-    } else if (percentile < 80 && percentile >= 70) {
-        return `🐎 Faster than ${percentile}% of ${suffix}`;
-    } else if (percentile < 70 && percentile >= 40) {
-        return `👍 Faster than ${percentile}% of ${suffix}`;
-    } else if (percentile < 40 && percentile >= 20) {
-        return `🐌 Slower than ${100 - percentile}% of ${suffix}`;
-    } else if (percentile <= 20 && percentile >= 10) {
-        return `🐌 Slower than ${100 - percentile}% of ${suffix}`;
-    } else {
-        return `🐌🐌🐌 Slower than ${100 - percentile}% of ${suffix}`;
-    }
-}
-
-enum EnvInputName {
-    TARGET_SECONDS = "target-seconds",
-    TARGET_PERCENTILE = "target-percentile",
-
-    TARGET_DEFAULT_SECONDS = "target-default-seconds",
-    TARGET_DEFAULT_PERCENTILE = "target-default-percentile",
-    TARGET_TAG_SECONDS = "target-tag-seconds",
-    TARGET_TAG_PERCENTILE = "target-tag-percentile",
-
-    TARGET_PR_PERCENTILE = "target-pr-percentile",
-    TARGET_PR_SECONDS = "target-pr-seconds",
-    TARGET_PR_FAILURE_PERCENTILE = "target-pr-failure-percentile",
-    TARGET_PR_FAILURE_SECONDS = "target-pr-failure-seconds",
-
-    TARGET_DEFAULT_SUCCESS_RATE = "target-default-success-rate",
-    TARGET_TAG_SUCCESS_RATE = "target-tag-success-rate",
-    TARGET_PR_SUCCESS_RATE = "target-pr-success-rate",
-}
-
-enum EnvInputValue {
-    targetSeconds = parseInt(process.env.TARGET_SECONDS ?? "60"),
-    targetPercentile = parseInt(process.env.TARGET_PERCENTILE ?? "50"),
-
-    targetDefaultSeconds = parseInt(process.env.TARGET_DEFAULT_SECONDS ?? `${targetSeconds}`),
-    targetDefaultPercentile = parseInt(process.env.TARGET_DEFAULT_PERCENTILE ?? `${targetPercentile}`),
-    targetTagSeconds = parseInt(process.env.TARGET_TAG_SECONDS ?? `${targetDefaultSeconds}`),
-    targetTagPercentile = parseInt(process.env.TARGET_TAG_PERCENTILE ?? `${targetDefaultPercentile}`),
-
-    targetPrPercentile = parseInt(process.env.TARGET_PR_PERCENTILE ?? `${targetPercentile}`),
-    targetPrSeconds = parseInt(process.env.TARGET_PR_SECONDS ?? `${targetSeconds}`),
-    targetPrFailurePercentile = parseInt(process.env.TARGET_PR_FAILURE_PERCENTILE ?? `${targetPrPercentile}`),
-    targetPrFailureSeconds = parseInt(process.env.TARGET_PR_FAILURE_SECONDS ?? `${targetPrSeconds}`),
-
-    targetDefaultSuccessRate = parseInt(process.env.TARGET_DEFAULT_SUCCESS_RATE ?? "99"),
-    targetTagSuccessRate = parseInt(process.env.TARGET_TAG_SUCCESS_RATE ?? `${targetDefaultSuccessRate}`),
-    targetPrSuccessRate = parseInt(process.env.TARGET_PR_SUCCESS_RATE ?? "90"),
-}
+import { EnvInputName, EnvInputValue } from "./src/inputs";
+import { describePercentile } from "./scr/ui";
 
 export async function summarizeHistory(args: GitHubScriptArguments): Promise<void> {
     const { github, context, core } = args;
